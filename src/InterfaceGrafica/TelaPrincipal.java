dea913b006208feee465bd9083e789c09fd5f889
Daniel <daniel.mad20@hotmail.com>
2026-09-23 21:30:48 -0300

    BaseDaInterface

---
+++ src/InterfaceGrafica/TelaPrincipal.java
@@ -0,0 +1,13 @@
+package InterfaceGrafica;
+
+import javax.swing.JFrame;
+public class TelaPrincipal extends JFrame {
+    public TelaPrincipal() {
+        setTitle("Sistema de Locação de Veículos");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 import pagamento.FormaPagamento;
 import pagamento.PagamentoPix;
 
-void main() {
+public class Main {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new InterfaceGrafica.TelaPrincipal().setVisible(true);
+            }
+        });
+        
+    }
 
 
 }
